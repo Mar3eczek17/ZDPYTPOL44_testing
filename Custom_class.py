6813226f7e89a0b483a16e7c98d485400c851336
Marek Grzesiak <marekgrzesiak.22@gmail.com>
2021-11-05 13:07:03 +0100

    Iterator class

--- Custom_class.py
+++ Custom_class.py
@@ -1,3 +1,55 @@
+class Team:
+   '''
+   Contains List of Junior and senior team members
+   '''
+   def __init__(self):
+       self._juniorMembers = list()
+       self._seniorMembers = list()
+   def addJuniorMembers(self, members):
+       self._juniorMembers += members
+   def addSeniorMembers(self, members):
+       self._seniorMembers += members
+
+
+# Create team class object
+team = Team()
+# Add name of junior team members
+team.addJuniorMembers(['Sam', 'John', 'Marshal'])
+# Add name of senior team members
+team.addSeniorMembers(['Riti', 'Rani', 'Aadi'])
+
+# iter(team)
+# for member in team:
+#    print(member)
+
+def __iter__(self):
+    pass
+
+
+class TeamIterator:
+   '''
+   Contains List of Junior and senior team members and also overrides the __iter__() function.
+   '''
+   def __init__(self):
+       self._juniorMembers = list()
+       self._seniorMembers = list()
+   def addJuniorMembers(self, members):
+       self._juniorMembers += members
+   def addSeniorMembers(self, members):
+       self._seniorMembers += members
+   def __iter__(self):
+       ''' Returns the Iterator object '''
+       return TeamIterator(self)
+
+
+# Get Iterator object from Iterable Team class object
+# iterator = iter(team)
+# print(iterator)
+
+def __next__(self):
+    pass
+
+
 class TeamIterator:
    ''' Iterator class '''
    def __init__(self, team):
@@ -16,42 +68,25 @@ class TeamIterator:
            return result
        # End of Iteration
        raise StopIteration
-class Team:
-   '''
-   Contains List of Junior and senior team members and also overrides the __iter__() function.
-   '''
-   def __init__(self):
-       self._juniorMembers = list()
-       self._seniorMembers = list()
-   def addJuniorMembers(self, members):
-       self._juniorMembers += members
-   def addSeniorMembers(self, members):
-       self._seniorMembers += members
-   def __iter__(self):
-       ''' Returns the Iterator object '''
-       return TeamIterator(self)
-def main():
-    # Create team class object
-    team = Team()
-    # Add name of junior team members
-    team.addJuniorMembers(['Sam', 'John', 'Marshal'])
-    # Add name of senior team members
-    team.addSeniorMembers(['Riti', 'Rani', 'Aadi'])
-    print('*** Iterate over the team object using for loop ***')
-    # Iterate over team object(Iterable)
-    for member in team:
-        print(member)
-    print('*** Iterate over the team object using while loop ***')
-    # Get Iterator object from Iterable Team class oject
-    iterator = iter(team)
-    # Iterate over the team object using iterator
-    while True:
-        try:
-            # Get next element from TeamIterator object using iterator object
-            elem = next(iterator)
-            # Print the element
-            print(elem)
-        except StopIteration:
-            break
-if __name__ == '__main__':
-  main()
+
+# Create team class object
+team = Team()
+# Add name of junior team members
+team.addJuniorMembers(['Sam', 'John', 'Marshal'])
+# Add name of senior team members
+team.addSeniorMembers(['Riti', 'Rani', 'Aadi'])
+# Get Iterator object from Iterable Team class oject
+iterator = iter(team)
+# Iterate over the team object using iterator
+while True:
+    try:
+        # Get next element from TeamIterator object using iterator object
+        elem = next(iterator)
+        # Print the element
+        print(elem)
+    except StopIteration:
+        break
+
+# Iterate over team object(Iterable)
+for member in team:
+    print(member)
